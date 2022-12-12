48ad4fc2166b7ef0fdde2e8e161a34b8d025cdac
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-12-12 19:18:43 +0100

    add guessword and its potentional synonyms to page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 let retries = 5;
+const guessWordElem = document.querySelector(".guess-word");
+const synonymsElem = document.querySelector(".synonyms");
 
 main();
 
@@ -8,17 +10,37 @@ function main() {
   getGuessWord()
     .then(guessWord => {
       retries = 5;
-      console.log("success", guessWord)
+
+      let synonymsHTML = getSynonymsHTML(guessWord);
+      guessWordElem.textContent = guessWord.word;
+      synonymsElem.innerHTML = synonymsHTML;
     })
     .catch(err => {
-      console.error("failure", `error ${err}`);
-      if (retries-- > 0) start();
+      console.error(`error ${err}`);
+      if (retries-- > 0) main();
     });
+}
+
+function getSynonymsHTML(guessWord) {
+  let options = [guessWord.synonymsPrimary[0]];
+  while (options.length < 4) {
+    let word = wordlist.get();
+    if (!guessWord.synonymsAll.includes(word)) options.push(word);
+  }
+  options.sort(() => 0.5 - Math.random);
+
+  let resultHTML = "";
+  while (options.length) {
+    let word = options.pop();
+    resultHTML += `<li class="synonym-item"><a class="synonym-link" href="#">${word}</a></li>\n`;
+  }
+
+  return resultHTML;
 }
 
 function getGuessWord() {
   const randomWord = wordlist.get();
-  return getSynonyms(randomWord); 
+  return getSynonyms(randomWord);
 }
 
 function getSynonyms(word) {
