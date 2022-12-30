363f9c8ce3618be7644378971a3976daad06c7ec
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-12-30 14:32:14 +0100

    add devtools source map configuration for webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const config = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    devtool: 'source-map'
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
